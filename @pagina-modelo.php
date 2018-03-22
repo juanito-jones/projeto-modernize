8450077792196935fb47ca96b53f0c9fd96250a0
Juan Rangel <juancfrangel@gmail.com>
2018-03-22 15:49:39 -0300

    .

--- @pagina-modelo.php
+++ @pagina-modelo.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Garden</title>
+		<title>Modernize</title>
 		<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 		<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700,800%7CLato:300,400,700" rel="stylesheet" type="text/css">
 		<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
