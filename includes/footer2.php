486cd38e0cb2a597e642c723730b7a85d2a03b8c
Juan Rangel <juancfrangel@gmail.com>
2018-03-22 16:36:31 -0300

    .

--- includes/footer2.php
+++ includes/footer2.php
@@ -258,28 +258,28 @@
     <div class="container">
         <div class="row footer-info mb-60">
             <div class="col-lg-3 col-md-4 col-xs-12 mb-sm-30">
-                <h4 class="mb-30 text-uppercase">CONTACT Us</h4>
+                <h4 class="mb-30 text-uppercase">CONTATO</h4>
                 <address>
-                    <i class="ion-ios-location fa-icons"></i> 123 Main Street, St. NW Ste, 1 Washington, DC,USA.
+                    <i class="ion-ios-location fa-icons"></i> Dr. Roberto Barrozo 1588 Bom Retiro Ctba PR 80520 070
                 </address>
                 <ul class="link-small">
-                    <li><a href="mailto:business@support.com"><i class="ion-ios-email fa-icons"></i>business@support.com</a></li>
-                    <li><a><i class="ion-ios-telephone fa-icons"></i>+56 (0) 012 345 6789</a></li>
+                    <li><a href="mailto:business@support.com"><i class="ion-ios-email fa-icons"></i> falecom@lojadopaisagista.com</a></li>
+                    <li><a><i class="ion-ios-telephone fa-icons"></i> 41 3022 1925</a></li>
                 </ul>
                 <div class="icons-hover-black">
                     <a href="javascript:avoid(0);"> <i class="fa fa-facebook"></i> </a><a href="javascript:avoid(0);"> <i class="fa fa-twitter"></i> </a><a href="javascript:avoid(0);"> <i class="fa fa-youtube"></i> </a><a href="javascript:avoid(0);"> <i class="fa fa-dribbble"></i> </a><a href="javascript:avoid(0);"> <i class="fa fa-linkedin"></i> </a>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-3 col-xs-12 mb-sm-30">
-                <h4 class="mb-30 text-uppercase">Links</h4>
+            <div class="col-lg-3 col-md-3 col-xs-12 mb-sm-30">
+                <h4 class="mb-30 text-uppercase">MAPA</h4>
                 <ul class="link blog-link">
-                    <li><a href="javascript:avoid(0);"><i class="fa fa-angle-double-right"></i> About Us</a></li>
-                    <li><a href="javascript:avoid(0);"><i class="fa fa-angle-double-right"></i> Services</a></li>
-                    <li><a href="javascript:avoid(0);"><i class="fa fa-angle-double-right"></i> Privacy policy</a></li>
-                    <li><a href="javascript:avoid(0);"><i class="fa fa-angle-double-right"></i> Terms &amp; condition</a></li>
+                    <li><a href="javascript:avoid(0);"><i class="fa fa-angle-double-right"></i> Sobre</a></li>
+                    <li><a href="javascript:avoid(0);"><i class="fa fa-angle-double-right"></i> Produtos</a></li>
+                    <li><a href="javascript:avoid(0);"><i class="fa fa-angle-double-right"></i> Politica de Privacidade</a></li>
+                    <li><a href="javascript:avoid(0);"><i class="fa fa-angle-double-right"></i> Termos & Condições</a></li>
                 </ul>
             </div>
-            <div class="col-lg-3 col-md-5 col-xs-12 mb-sm-30">
+<!--            <div class="col-lg-3 col-md-5 col-xs-12 mb-sm-30">
                 <h4 class="mb-30 text-uppercase">Latest Blog</h4>
                 <div class="widget-details link">
                     <div class="post-type-post media mb-xs-30">
@@ -304,7 +304,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>-->
             <div class="col-lg-4 col-md-12 col-xs-12 mt-sm-30 mt-xs-30">
                 <div class="newsletter">
                     <h4 class="mb-30 text-uppercase">Newsletter Signup</h4>
--- includes/header2.php
+++ includes/header2.php
@@ -23,15 +23,9 @@
 				<div class="col-lg-8">
 					<div class="top_loction pull-left">
 						<ul>
-							<li>
-								<a href="#!"><i class="fa fa-map-marker"></i> Rua Dr. Roberto Barrozo, 1588 Bom Retiro Curitiba</a>
-							</li>
-							<li>
-								<a href="mailto:Support@Domain.Com"><i class="fa fa-envelope"></i> Support@Domain.Com</a>
-							</li>
-							<li>
-								<a href="tel:1234567890"><i class="fa fa-phone"></i> +91 123 456 7890</a>
-							</li>
+							<li><a href="#!"><i class="fa fa-map-marker"></i> Rua Dr. Roberto Barrozo, 1588 Bom Retiro Curitiba</a></li>
+							<li><a href="mailto:Support@Domain.Com"><i class="fa fa-envelope"></i> falecom@lojadopaisagista.com</a></li>
+							<li><a href="tel:1234567890"><i class="fa fa-phone"></i> (41) 3022 1925</a></li>
 						</ul>
 					</div>
 				</div>
@@ -65,81 +59,39 @@
 					<div class="col-md-9 nav-bg">
 						<nav class="navigation">
 							<ul>
-								<li>
-									<a href="#!">Home</a><i class="ion-ios-plus-empty hidden-md-up"></i>
-									<ul class="sub-nav">
-										<li>
-											<a href="index.php">Home Page 1</a>
-										</li>
-										<li>
-											<a href="index2.php">Home Page 2</a>
-										</li>
-										<li>
-											<a href="index3.php">Home Page 3</a>
-										</li>										<li>
-										<a href="index4.php">Home Page 4</a>
-										</li>
+								<li><a href="#!">PÁGINA INICIAL</a><i class="ion-ios-plus-empty hidden-md-up"></i>
+                                    <ul class="sub-nav">
+										<li><a href="index.php">Home Page 1</a></li>
+										<li><a href="index2.php">Home Page 2</a></li>
+										<li><a href="index3.php">Home Page 3</a></li>										
+                                        <li><a href="index4.php">Home Page 4</a></li>
 									</ul>
 								</li>
-								<li>
-									<a href="about.php">About</a>
-								</li>
-								<li>
-									<a href="javascript:avoid(0);">services</a><i class="ion-ios-plus-empty hidden-md-up"></i>
-									<ul class="sub-nav">
-										<li>
-											<a href="services.php">All Services</a>
-										</li>
-										<li>
-											<a href="services-details.php">Services Details</a>
-										</li>
-
+								<li><a href="about.php">QUEM SOMOS</a></li>
+								<li><a href="javascript:avoid(0);">PERSIANAS</a><i class="ion-ios-plus-empty hidden-md-up"></i>
+                                    <ul class="sub-nav">
+										<li><a href="services.php">All Services</a></li>
+										<li><a href="services-details.php">Services Details</a></li>
 									</ul>
 								</li>
-
-								<li>
-									<a href="javascript:avoid(0);">blog</a><i class="ion-ios-plus-empty hidden-md-up"></i>
-									<ul class="sub-nav">
-										<li>
-											<a href="blog.php">Blog</a>
-										</li>
-										<li>
-											<a href="blog-detail.php">Blog Details</a>
-										</li>
+								<li><a href="javascript:avoid(0);">CORTINAS</a><i class="ion-ios-plus-empty hidden-md-up"></i>
+                                    <ul class="sub-nav">
+										<li><a href="blog.php">Blog</a></li>
+										<li><a href="blog-detail.php">Blog Details</a></li>
 									</ul>
 								</li>
-								<li>
-									<a href="javascript:avoid(0);">pages</a><i class="ion-ios-plus-empty hidden-md-up"></i>
-									<ul class="sub-nav">
-
-										<li>
-											<a href="team.php">Team</a>
-										</li>
-										<li>
-											<a href="faq.php">Faq</a>
-										</li>
-										<li>
-											<a href="register.php">Register</a>
-										</li>
-										<li>
-											<a href="login.php">login</a>
-										</li>
-										<li>
-											<a href="testimonial.php">Testimonials</a>
-										</li>
-										<li>
-											<a href="404.php">404 Error</a>
-										</li>
-										<li>
-											<a href="blank.php">blank</a>
-										</li>
+								<li><a href="javascript:avoid(0);">OMBRELONES</a><i class="ion-ios-plus-empty hidden-md-up"></i>
+                                    <ul class="sub-nav">
+										<li><a href="team.php">Team</a></li>
+										<li><a href="faq.php">Faq</a></li>
+										<li><a href="register.php">Register</a></li>
+										<li><a href="login.php">login</a></li>
+										<li><a href="testimonial.php">Testimonials</a></li>
+										<li><a href="404.php">404 Error</a></li>
+										<li><a href="blank.php">blank</a></li>
 									</ul>
 								</li>
-
-								<li>
-									<a href="contact.php">Contact us</a>
-								</li>
-
+								<li><a href="contact.php">CONTATO</a></li>
 							</ul>
 						</nav>
 					</div>
